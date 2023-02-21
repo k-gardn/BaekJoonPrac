48901a7777163d8fa8ff917538cda1be46debe96
Jeongwon <108037773+k-gardn@users.noreply.github.com>
2023-02-21 11:10:06 +0900

    Update 최댓값과 최솟값.js

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,9 +1,8 @@
 function solution(s) {
     var answer = '';
     const strArr = s.split(' ').sort((a,b)=>a-b)
-    // .filter((el,idx)=> el[0],el[])
-    // console.log(strArr.shift(),strArr.pop())
     answer = [strArr.shift(),strArr.pop()].join(' ')
-    // answer += strArr.shift() + strArr.pop()
+    //const strArr = s.split(' ').sort((a,b)=>a-b).filter((el,idx,arr)=> idx === 0 || idx === arr.length-1)
+    //answer = strArr.join(' ')
     return answer;
 }
