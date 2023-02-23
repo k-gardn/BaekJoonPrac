e0c6578fd88b70494e57eaa51617003c9edcbb83
Jeongwon <108037773+k-gardn@users.noreply.github.com>
2023-02-23 15:57:07 +0900

    [level 2] Title: 숫자의 표현, Time: 2.11 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -3,18 +3,22 @@ function solution(n) {
     
     for(let i=1;i<=parseInt(n/2);i++){
         let sum =i
-        for(let j=i+1;j<=parseInt(n/2)+1;j++){
-            // console.log('i',i,'j',j)
-                sum += j
-                // console.log('sum',sum)
-                if(sum === n){
-                    // console.log(i,j)
+        let plusNum = i+1
+        while(sum <= n){
+            sum += plusNum
+            ++plusNum
+            if(sum === n){
                     ++answer
-                    // console.log('answer',answer)
                 }
-                if(sum >=n) break
-            
+            // console.log(plusNum)
         }
+        // for(let j=i+1;j<=parseInt(n/2)+1;j++){
+        //         sum += j
+        //         if(sum === n){
+        //             ++answer
+        //         }
+        //         if(sum >n) break
+        // }
     }
     return answer;
 }
